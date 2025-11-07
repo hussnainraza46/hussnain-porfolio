9c4b727d412f543d49eab43769baabedf98de75f
Hussnain Raza <mirxahusnain75@gmail.com>
2025-11-07 15:15:32 +0500

    Auto update

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  base: '/hussnain-porfolio/', // 👈 IMPORTANT: must match your repo name exactly
+  plugins: [react(), tailwindcss()],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
         mono: ["Momo Trust Sans", "sans-serif"],
-        nurito: [ "Nunito", "sans-serif"]
+        nurito: ["Nunito", "sans-serif"],
       },
       colors: {
         primary: "#06b6d4", // cyan
@@ -18,7 +18,4 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(),
-     tailwindcss()
-  ],
 })
