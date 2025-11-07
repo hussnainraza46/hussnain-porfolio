858324b4dec6d336a1dcdfae1f77f212d99a57b9
Hussnain Raza <mirxahusnain75@gmail.com>
2025-11-07 15:17:24 +0500

    Auto update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="text-center py-6 bg-gray-900 text-gray-400">
-      © {new Date().getFullYear()} Hussnain Raza okay | All Rights Reserved
+      © {new Date().getFullYear()} Hussnain Raza  | All Rights Reserved
     </footer>
   );
 }
